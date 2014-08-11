d9a504b40dedec0b62c81517787548dfb81efd46
Fernando Pereira <fernandopereirala@gmail.com>
2014-08-11 17:59:27 -0300

    Add create and delete methods in Droplet Class.

--- lib/provider/digital_ocean/droplet.rb
+++ lib/provider/digital_ocean/droplet.rb
@@ -14,10 +14,24 @@ module Provider
       tratar_response(response)
     end
 
+    def create
+      @header.merge!({'Content-Type' => "application/json"})
+      query = {name: "Default", region: "sfo1", size: "512mb", image: '5141286'}
+      response = self.class.post('/droplets', {query: query, headers: @header})
+      tratar_response(response)
+    end
+
+    def delete(id)
+      @header.merge!({'Content-Type' => "application/x-www-form-urlencoded"})
+      response = self.class.delete("/droplets/#{id}", headers: @header)
+      tratar_response(response)
+    end
+
   private
     
     def tratar_response(response)
       if response.code == 200
+        puts response
         response.parsed_response
       else
         raise ArgumentError, response.parsed_response['error']
--- lib/provider/digital_ocean_v2.rb
+++
@@ -1,4 +0,0 @@
-require "provider/digital_ocean_v2/droplet"
-
-module Provider::DigitalOceanV2
-end
