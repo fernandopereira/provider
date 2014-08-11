99814b82dbb560fa3160d78ab93d06f5cf5872a7
Fernando Pereira <fernandopereirala@gmail.com>
2014-08-11 16:19:27 -0300

    Ajustes de requires.

--- lib/provider.rb
+++ lib/provider.rb
@@ -3,7 +3,7 @@ require 'json'
 
 require "provider/version"
 require "provider/digital_ocean"
-require "provider/digital_ocean_v2/droplet"
+require "provider/digital_ocean_v2"
 
 module Provider
 end
--- lib/provider/digital_ocean_v2.rb
+++ lib/provider/digital_ocean_v2.rb
@@ -1,2 +1,4 @@
+require "provider/digital_ocean_v2/droplet"
+
 module Provider::DigitalOceanV2
 end
