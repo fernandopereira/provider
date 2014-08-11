595f0f4860cf520dc4984b8dce142545768ee5f4
Fernando Pereira <fernandopereirala@gmail.com>
2014-08-11 16:36:44 -0300

    Ajustes de requires.

--- lib/provider.rb
+++ lib/provider.rb
@@ -3,7 +3,7 @@ require 'json'
 
 require "provider/version"
 require "provider/digital_ocean"
-require "provider/digital_ocean_v2"
+require "provider/digital_ocean/droplet"
 
 module Provider
 end
---
+++ lib/provider/digital_ocean/droplet.rb
@@ -0,0 +1,27 @@
+module Provider
+  class DigitalOcean::Droplet
+    include HTTParty
+    base_uri 'https://api.digitalocean.com/v2'
+
+    def initialize(api_token)
+      @api_token = api_token
+      @header = {'Authorization' => "Bearer #{@api_token}"}
+    end
+
+    #List all droplets created
+    def list
+      response = self.class.get('/droplets', headers: @header)
+      tratar_response(response)
+    end
+
+  private
+    
+    def tratar_response(response)
+      if response.code == 200
+        JSON.parse(response.parsed_response)
+      else
+        raise ArgumentError, response.parsed_response['error']
+      end
+    end
+  end
+end
